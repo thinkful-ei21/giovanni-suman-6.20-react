08b73d9426244ac31f343445d1a9c41591270e90
Suman Bandita <sumanbandita@gmail.com>
2018-06-20 13:48:30 -0400

    css for gif

--- giphy-reader/src/Gif.js
+++ giphy-reader/src/Gif.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './Gif.css';
 
 export default function Gif(props){
     console.log(props.example);
 return (    
-    <div>
+    <div className="gif">
         <label>{props.example.data[1].title}</label>
         <img src="https://media0.giphy.com/media/95xRbt5dxppWE/200w.gif?cid=e1bb72ff5b2a73224d6e4835512fa36d"></img>        
     </div>    
